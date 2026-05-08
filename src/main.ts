d45ea69ff8aa90bed9b80a194799b780d2776b52
[TC-HC] <[giddlfdka12345@gmail.com]>
2026-05-09 00:17:38 +0900

    Making Database

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {  // 서버를 시작하는 함수
   .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('default', app, document);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   await app.listen(process.env.PORT ?? 3000); // Port가 지정되어 있다면 그곳에서 서버 실행, 그렇지 않을 경우 3000포트에서 서버 실행
